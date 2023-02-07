9d4fa54725ad50770fafc5258a808b1ae389c428
ace <kaurrajvinder736@gmail.com>
2023-02-07 22:51:38 +0530

    Template initiated

---
+++ src/pages/Landingpage.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from '../components/Header';
+import Home from '../components/Home';
+import About from '../components/About';
+import Whatwedo from '../components/Whatwedo';
+import Imagegallery from '../components/Imagegallery';
+import Collabs from '../components/Collabs';
+import Contacts from '../components/Contacts';
+import Footer from '../components/Footer';
+
+
+
+function Landingpage() {
+  return (
+    <>
+     <Header/>
+     <Home />
+     <About />
+     <Whatwedo />
+     <Imagegallery />
+     <Collabs />
+     <Contacts />
+     <Footer />
+    </>
+  )
+}
+
+export default Landingpage
